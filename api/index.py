fbcf83b3ec32d8c0cbf922e3003ead37ad9da077
Fazalunnisa <fazalunnisa17@gmail.com>
2026-04-12 21:02:55 +0000

    emergency fix

--- api/index.py
+++ api/index.py
@@ -1,7 +1,1 @@
-import os
-import sys
-
-# Add the parent directory to sys.path so we can import from the 'fastapi' folder
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from fastapi.main import app
